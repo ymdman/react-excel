595748ccf1ec9da90812e05b0aaa7af630d40a4f
ymdman <revitalized@icloud.com>
2017-08-11 21:54:53 +0900

    :art: Table.jsをclassに変更

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './Header';
+import Table from './Table';
 
 const headerItems = [
   { title: 'タイトル', index: 0 },
@@ -9,8 +9,18 @@ const headerItems = [
   { title: '売り上げ部数', index: 4 },
 ];
 
+const tableData = [
+  { data: ['The Lord of the Rings', 'J. R. R. Tolkien', 'English', '1954-1955', '150 million'], index: 0 },
+  { data: ['Le Petit Prince (The Little Prince)', 'Antoine de Saint-Exupéry', 'French', '1943', '140 million'], index: 1 },
+  { data: ['Harry Potter and the Philosophers Stone', 'J. K. Rowling', 'English', '1997', '107 million'], index: 2 },
+  { data: ['And Then There Were None', 'Agatha Christie', 'English', '1939', '100 million'], index: 3 },
+  { data: ['Dream of the Red Chamber', 'Cao Xueqin', 'Chinese', '1754-1791', '100 million'], index: 4 },
+  { data: ['The Hobbit', 'J. R. R. Tolkien', 'English', '1937', '100 million'], index: 5 },
+  { data: ['She: A History of Adventure', 'H. Rider Haggard', 'English', '1887', '100 mixllio'], index: 6 },
+];
+
 const App = () => (
-  <Header headerItems={headerItems} />
+  <Table headerItems={headerItems} tableData={tableData} />
 );
 
 export default App;
---
+++ src/components/Table.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+export default class Table extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tableData: this.props.tableData,
+    };
+  }
+
+
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            {this.props.headerItems.map(headerItems => (
+              <th key={headerItems.index}>{headerItems.title}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {this.props.tableData.map(tableData => (
+              <td key={tableData.index}>{tableData.data}</td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+// const Table = props => (
+//   <table className="table">
+//     <thead>
+//       <tr>
+//         {props.headerItems.map(headerItems => (
+//           <th key={headerItems.index}>{headerItems.title}</th>
+//         ))}
+//       </tr>
+//     </thead>
+//     <tbody>
+//       <tr>
+//         {props.tableData.map(tableData => (
+//           <td key={tableData.index}>{tableData.data}</td>
+//         ))}
+//       </tr>
+//     </tbody>
+//   </table>
+// );
+
+Table.propTypes = {
+  headerItems: React.PropTypes.arrayOf(React.PropTypes.object),
+  tableData: React.PropTypes.arrayOf(React.PropTypes.object),
+};
+
+Table.defaultProps = {
+  headerItems: [],
+  tableData: [],
+};
+
+// export default Table;
